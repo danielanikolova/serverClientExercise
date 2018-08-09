e4bf7ed056eee3bdd11ca1b63951e1924ed8a864
Daniela Nikolova <d.nikolova@seeburger.com>
2018-08-09 17:49:08 +0300

    last changes

--- FTPLibrary/src/lib/MyCrypto.java
+++ FTPLibrary/src/lib/MyCrypto.java
@@ -1,6 +1,7 @@
 package lib;
 
 import java.io.UnsupportedEncodingException;
+import java.nio.ByteBuffer;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
@@ -11,41 +12,7 @@ import javax.xml.bind.DatatypeConverter;
 
 public class MyCrypto {
 
-	//Returns a random salt
-	public static String generateRandomSalt() {
-		byte[] salt = new byte[8];
-		try {
-			SecureRandom.getInstanceStrong().nextBytes(salt);
-		} catch (NoSuchAlgorithmException e) {
-			e.printStackTrace();
-		}
-		return salt.toString();
-	}
-
-	// returns random iterations between 1024 and 4096
-	public static int getRandomIterations() {
-
-		Random random = new Random();
-		int maximum = 4096;
-		int minimum = 1024;
-
-		int n = maximum - minimum + 1;
-		int i = random.nextInt() % n;
-		return  minimum + i;
-	}
-
-	//this method concatenates hashed password and salt iterations count of time
-	public static String saltParameter(String salt, String parameter, int iterations) {
-		String result = parameter;
-
-		for (int i = 0; i < iterations; i++) {
-			result = generateHash(salt + result);
-		}
-
-		return result;
-	}
-
-	//returns hashed password
+	//generate hash from string
 	public static String generateHash(String input) {
 		String sha256 = "";
 		try {
@@ -71,29 +38,46 @@ public class MyCrypto {
 		return result;
 	}
 
-	public static String saltRegistrationPass(String salt, int iterations) {
-
-		StringBuilder sb = new StringBuilder();
-		sb.append(FTPConstants.REGISTRATION_PASS);
+	//salts password iterations count of time
+	public static String saltPassword(String salt, String passwordHash, int iterations) {
+		String result = passwordHash;
 
 		for (int i = 0; i < iterations; i++) {
-			sb.append(salt);
+			result = generateHash(salt + result);
 		}
 
-		return sb.toString();
+		return result;
 	}
 
-	//encode base64
-	public static String encodeBase64(String input) {
+	//generates random salt
+	public static byte[] generateRandomSalt() {
+		byte[] salt = new byte[8];
+		try {
+			SecureRandom.getInstanceStrong().nextBytes(salt);
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
 
-		return DatatypeConverter.printBase64Binary(input.getBytes());
+		return salt;
+
 	}
-	//decode base64
-	public static String decodeBase64(String input) {
 
-		return new String(DatatypeConverter.parseBase64Binary(input));
+	// generates random number of iterations between 4096 and 1024
+	public static int getRandomIterations() {
+		Random random = new Random();
+		int maximum = 4096;
+		int minimum = 1024;
+
+		return random.nextInt((maximum - minimum) + 1) + minimum;
+
 	}
 
+
+	public static byte[] intToBytes( final int i ) {
+	    ByteBuffer bb = ByteBuffer.allocate(4);
+	    bb.putInt(i);
+	    return bb.array();
+	}
 
 
 }
